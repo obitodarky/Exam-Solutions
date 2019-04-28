73e427a8b38620685dee9454e27a53a9c6a58dba
Harsh Mehta <deadspaceharsh@gmail.com>
2019-04-28 14:28:14 +0530

    awt end sem solutions

---
+++ AWT/In-Sem/End-Sem/connect.php
@@ -0,0 +1,24 @@
+<?php 
+
+$user = 'root';
+$password = 'root';
+$db = 'inventory';
+$host = 'localhost';
+$port = 8889;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link,
+   $host,
+   $user,
+   $password,
+   $db,
+   $port
+);
+
+if($link){
+    echo "Successful";
+
+} else {
+    echo "Could not connect";
+}
